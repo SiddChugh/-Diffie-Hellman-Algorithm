87cd64a62cf7c9c3827f9238b23db401f8f9947f
schugh <schugh@sfu.ca>
2020-07-02 18:38:22 -0700

    Fix Indent.

--- diffieHellmanKeyDelivery.java
+++ diffieHellmanKeyDelivery.java
@@ -4,59 +4,59 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Random;
 
- public class diffieHellmanKeyDelivery 
- {
- 	private static final int PRIVATE_NUMBER_MAX_VALUE = 10000;
-	
-	private BigInteger n;
-	private BigInteger q;
-	
-	private int privateNumber;
-	
-	private host h;
+public class diffieHellmanKeyDelivery 
+{
+  private static final int PRIVATE_NUMBER_MAX_VALUE = 10000;
+  
+  private BigInteger n;
+  private BigInteger q;
+  
+  private int privateNumber;
+  
+  private host h;
 
-	private static final int MIN_PRIME_VALUE = 1001;
-	private static final int PRIME_VALUE_RANGE = 10000;
+  private static final int MIN_PRIME_VALUE = 1001;
+  private static final int PRIME_VALUE_RANGE = 10000;
 
-	
-	/**
-	  *Flag specifying whether the host is a client or a server. 
-	  *Implementation details can be found in client.java and server.java
-	  */
-	private boolean isClient;
+  
+  /**
+    *Flag specifying whether the host is a client or a server. 
+    *Implementation details can be found in client.java and server.java
+    */
+  private boolean isClient;
 
-	public diffieHellmanKeyDelivery(host h, boolean isClient)
+  public diffieHellmanKeyDelivery(host h, boolean isClient)
   {
-		this.isClient = isClient;
-		this.h = h;
-		initializePrivateNumber();
-	}
+    this.isClient = isClient;
+    this.h = h;
+    initializePrivateNumber();
+  }
 
-	private void initializePrivateNumber() 
+  private void initializePrivateNumber() 
   {
-		Random rand = new Random();
-		privateNumber = rand.nextInt(PRIVATE_NUMBER_MAX_VALUE) + 2;
-	}
+    Random rand = new Random();
+    privateNumber = rand.nextInt(PRIVATE_NUMBER_MAX_VALUE) + 2;
+  }
 
-	/**
-	  *Generates a prime number within the range of [1001, 11001].
-	  */
-	public static int generatePrimeNumber() 
+  /**
+    *Generates a prime number within the range of [1001, 11001].
+    */
+  public static int generatePrimeNumber() 
   {
-		int candidate = generateCandidateNumber();
-		while (!isPrime(candidate)) {
-			candidate++;
-		}
-		return candidate;
-	}
-	
-	private static int generateCandidateNumber()
+    int candidate = generateCandidateNumber();
+    while (!isPrime(candidate)) {
+      candidate++;
+    }
+    return candidate;
+  }
+  
+  private static int generateCandidateNumber()
   {
-		Random rand = new Random();
-		return rand.nextInt(PRIME_VALUE_RANGE) + MIN_PRIME_VALUE;
-	}
-	
-	private static boolean isPrime(int n)
+    Random rand = new Random();
+    return rand.nextInt(PRIME_VALUE_RANGE) + MIN_PRIME_VALUE;
+  }
+  
+  private static boolean isPrime(int n)
   {
     if (n % 2 == 0) return n == 2;
     if (n % 3 == 0) return n == 3;
@@ -69,65 +69,65 @@ import java.util.Random;
       }
     }
     return true;
-	}
+  }
 
-	/**
-	  *Main algorithm performing the diffie-hellman encryption. 
-	  */
-	public int calculateKey() throws IOException 
+  /**
+    *Main algorithm performing the diffie-hellman encryption. 
+    */
+  public int calculateKey() throws IOException 
   {
-		sharePublicPrimeNumbers();
-		calculateAndSendMyPublicNumber();
-		BigInteger otherPublicNumber = readOtherPublicNumber();
-		
-		return calculatePrivateKey(otherPublicNumber);
-	}
-	
-	/**
-	  *Share the public primes. If the host is the client, it sends a message to 
+    sharePublicPrimeNumbers();
+    calculateAndSendMyPublicNumber();
+    BigInteger otherPublicNumber = readOtherPublicNumber();
+    
+    return calculatePrivateKey(otherPublicNumber);
+  }
+  
+  /**
+    *Share the public primes. If the host is the client, it sends a message to 
     *the server containing the primes. If the host is the server, it receives 
     *the primes from the client.
-	  */
-	private void sharePublicPrimeNumbers() throws IOException 
+    */
+  private void sharePublicPrimeNumbers() throws IOException 
   {
-		if (isClient) 
+    if (isClient) 
     {
-			n = BigInteger.valueOf(generatePrimeNumber());
-			q = BigInteger.valueOf(generatePrimeNumber());
-			h.sendMessage(n.toString() + " " + q.toString());
-		} else 
+      n = BigInteger.valueOf(generatePrimeNumber());
+      q = BigInteger.valueOf(generatePrimeNumber());
+      h.sendMessage(n.toString() + " " + q.toString());
+    } else 
     {
-			String publicNumbers = h.readMessage();
-			n = new BigInteger(publicNumbers.split(" ")[0]);
-			q = new BigInteger(publicNumbers.split(" ")[1]);
-		}
-	}
-	
-	/**
+      String publicNumbers = h.readMessage();
+      n = new BigInteger(publicNumbers.split(" ")[0]);
+      q = new BigInteger(publicNumbers.split(" ")[1]);
+    }
+  }
+  
+  /**
     *Calculates the public number for the host and sends it to the other host.
-	  */
-	private void calculateAndSendMyPublicNumber() throws IOException 
+    */
+  private void calculateAndSendMyPublicNumber() throws IOException 
   {
-		BigInteger myPublicNumber = calculatePublicNumber();
-		h.sendMessage(myPublicNumber.toString());
-	}
-	
-	private BigInteger calculatePublicNumber() 
+    BigInteger myPublicNumber = calculatePublicNumber();
+    h.sendMessage(myPublicNumber.toString());
+  }
+  
+  private BigInteger calculatePublicNumber() 
   {
-		return n.pow(privateNumber).mod(q);
-	}
-	
-	/**
-	  *Read the public number sent from the other host.
-	  */
-	private BigInteger readOtherPublicNumber() throws IOException 
+    return n.pow(privateNumber).mod(q);
+  }
+  
+  /**
+    *Read the public number sent from the other host.
+    */
+  private BigInteger readOtherPublicNumber() throws IOException 
   {
-		String message = h.readMessage();
-		return new BigInteger(message);
-	}
-	
-	private int calculatePrivateKey(BigInteger publicKey) 
+    String message = h.readMessage();
+    return new BigInteger(message);
+  }
+  
+  private int calculatePrivateKey(BigInteger publicKey) 
   {
-		return publicKey.pow(privateNumber).mod(q).intValue();
-	}
+    return publicKey.pow(privateNumber).mod(q).intValue();
+  }
  }
--- server.java
+++ server.java
@@ -11,142 +11,142 @@ import java.net.UnknownHostException;
 
 public class server implements host 
 {
-	private static final int DEFAULT_PORT = 12323;
-	private ServerSocket serverSocket;	
-	private Socket socket;
-	
-	/**
-	  *Open a new connection on the default port.
-	  */
-	public server() throws IOException 
+  private static final int DEFAULT_PORT = 12323;
+  private ServerSocket serverSocket;  
+  private Socket socket;
+  
+  /**
+    *Open a new connection on the default port.
+    */
+  public server() throws IOException 
   {
-		new server(DEFAULT_PORT);
-	}
+    new server(DEFAULT_PORT);
+  }
 
-	/**
-	  *Open a new connection on the specified port.
-	  */
-	public server(int port) throws IOException 
+  /**
+    *Open a new connection on the specified port.
+    */
+  public server(int port) throws IOException 
   {
-		serverSocket = new ServerSocket(port);
-	}
+    serverSocket = new ServerSocket(port);
+  }
 
-	/**
-	  *Run the server.
-	  */
-	public void run() throws IOException 
+  /**
+    *Run the server.
+    */
+  public void run() throws IOException 
   {
-		while (true) 
-    {	
-			acceptConnection();
-			int privateKey = getPrivateKey();
-			String cipher = readMessage();
-			String decryption = decryptMessage(cipher, privateKey);
-			printMessages(cipher, decryption);
-			socket.close();
-		}
-	}
+    while (true) 
+    { 
+      acceptConnection();
+      int privateKey = getPrivateKey();
+      String cipher = readMessage();
+      String decryption = decryptMessage(cipher, privateKey);
+      printMessages(cipher, decryption);
+      socket.close();
+    }
+  }
 
-	/**
-	  *Wait for a connection to the server socket from the client and accept it.
-	  */
-	private void acceptConnection() throws IOException 
+  /**
+    *Wait for a connection to the server socket from the client and accept it.
+    */
+  private void acceptConnection() throws IOException 
   {
-		System.out.println(getInetAddress() + " Listening on port " + 
+    System.out.println(getInetAddress() + " Listening on port " + 
     serverSocket.getLocalPort() + "...");
-		
+    
     this.socket = serverSocket.accept();
-		
+    
     System.out.println("Accepted connection from " + 
     socket.getRemoteSocketAddress());
-	}
-	
-	private int getPrivateKey() throws IOException 
+  }
+  
+  private int getPrivateKey() throws IOException 
   {
-		diffieHellmanKeyDelivery creator = 
+    diffieHellmanKeyDelivery creator = 
     new diffieHellmanKeyDelivery(this, false);
-		return creator.calculateKey();
-	}
-	
-	/**
-	  *Get the local IPv4 address.
-	  */
-	private String getInetAddress() 
+    return creator.calculateKey();
+  }
+  
+  /**
+    *Get the local IPv4 address.
+    */
+  private String getInetAddress() 
   {
-		try 
+    try 
     {
-			return Inet4Address.getLocalHost().getHostAddress();
-		} catch (UnknownHostException e) 
+      return Inet4Address.getLocalHost().getHostAddress();
+    } catch (UnknownHostException e) 
     {
-			return "";
-		}
-	}
-	
-	/**
-	  *Read the input from the client and prints the data in UTF format.
-	  */
-	public String readMessage() throws IOException 
+      return "";
+    }
+  }
+  
+  /**
+    *Read the input from the client and prints the data in UTF format.
+    */
+  public String readMessage() throws IOException 
   {
-		DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-		return inputStream.readUTF();
-	}
-	
-	/**
-	  *Decrypts the cipher sent by the client. 
-	  */
-	private String decryptMessage(String cipher, int privateKey) 
+    DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+    return inputStream.readUTF();
+  }
+  
+  /**
+    *Decrypts the cipher sent by the client. 
+    */
+  private String decryptMessage(String cipher, int privateKey) 
   {
-		encryption scheme = new encryption(privateKey);
-		return scheme.decrypt(cipher);
-	}
-	
-	/**
-	  *Print the encrypted and decrypted messages.
-	  */
-	private void printMessages(String cipher, String decryption) 
+    encryption scheme = new encryption(privateKey);
+    return scheme.decrypt(cipher);
+  }
+  
+  /**
+    *Print the encrypted and decrypted messages.
+    */
+  private void printMessages(String cipher, String decryption) 
   {
-		System.out.println("\nReceived encrypted message:\n" + cipher + "\n");
-		System.out.println("Decrypted:\n" + decryption + "\n");
-	}
-	
-	/**
-	  *Sends a message to the client.
-	  */
-	public void sendMessage(String message) throws IOException 
+    System.out.println("\nReceived encrypted message:\n" + cipher + "\n");
+    System.out.println("Decrypted:\n" + decryption + "\n");
+  }
+  
+  /**
+    *Sends a message to the client.
+    */
+  public void sendMessage(String message) throws IOException 
   {
-		DataOutputStream outputStream = 
+    DataOutputStream outputStream = 
     new DataOutputStream(socket.getOutputStream());
     outputStream.writeUTF(message);
-	}
-	
-	/**
-	  *Retrieve the port from the args array.
-	  */
-	private static int getPortFromArgs(String[] args) 
+  }
+  
+  /**
+    *Retrieve the port from the args array.
+    */
+  private static int getPortFromArgs(String[] args) 
   {
-		if (args.length > 0) 
+    if (args.length > 0) 
     {
-			try 
+      try 
       {
-				return Integer.parseInt(args[0]);
-			} catch (NumberFormatException e) 
+        return Integer.parseInt(args[0]);
+      } catch (NumberFormatException e) 
       {
-				return DEFAULT_PORT;
-			}
-		}
-		return DEFAULT_PORT;
-	}
-	
-	public static void main(String[] args) 
+        return DEFAULT_PORT;
+      }
+    }
+    return DEFAULT_PORT;
+  }
+  
+  public static void main(String[] args) 
   {
-		int port = getPortFromArgs(args);
-		try 
+    int port = getPortFromArgs(args);
+    try 
     {
-			server s = new server(port);
-			s.run();
-		} catch (IOException e) 
+      server s = new server(port);
+      s.run();
+    } catch (IOException e) 
     {
-			e.printStackTrace();
-		}
-	}
+      e.printStackTrace();
+    }
+  }
 }
